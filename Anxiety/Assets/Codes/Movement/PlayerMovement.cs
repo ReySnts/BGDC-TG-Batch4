b682f23f30cd8a6472a9b97f634b7371a1c7f379
ReySnts <76163718+ReySnts@users.noreply.github.com>
2022-06-05 00:59:34 +0700

    Sound

--- Anxiety/Assets/Codes/Movement/PlayerMovement.cs
+++ Anxiety/Assets/Codes/Movement/PlayerMovement.cs
@@ -15,21 +15,34 @@ public class PlayerMovement : MonoBehaviour
     [Range(0f, 20f)] public float jumpForce = 7.5f;
     [Header("Animation")]
     public Animator playerControlAnim = null;
-    [SerializeField] bool leftTurn = false;
-    [SerializeField] bool rightTurn = false;
+    bool leftTurn = false;
+    bool rightTurn = false;
+    [SerializeField] bool isCrouch = false;
+    [SerializeField] bool isRun = false;
+    [SerializeField] bool startJump = false;
+    [SerializeField] bool startLand = false;
     bool firstRightWalk = false;
-    bool isCrouch = false;
     float tempX = 0f;
+    float tempZ = 0f;
     [Header("Masking")]
     public Transform groundCheck = null;
     public LayerMask groundMask;
     [SerializeField] bool isGround = false;
-    [SerializeField] float groundMaxDist = 0.06f;
+    float groundMaxDist = 0.59f;
+    [Header("Sounds")]
+    [SerializeField] bool isPressed = false;
+    public AudioSource walkSound = null;
+    public AudioSource crouchSound = null;
+    public AudioSource runSound = null;
+    public AudioSource jumpSound = null;
+    public AudioSource landSound = null;
     [Header("Event")]
     public UnityEvent OnLand = null;
     public void Landing()
     {
+        landSound.Play();
         playerControlAnim.SetBool("IsJumping", false);
+        startLand = false;
     }
     void Start()
     {
@@ -43,6 +56,18 @@ public class PlayerMovement : MonoBehaviour
         // Walk
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
+        if (!isPressed && (x != 0f || z != 0f))
+        {
+            walkSound.Play();
+            isPressed = true;
+        }
+        else if (x == 0f && z == 0f)
+        {
+            walkSound.Stop();
+            runSound.Stop();
+            isPressed = false;
+        }
+        // Turn Left or Right
         if (Input.GetKeyDown(KeyCode.A) && x - tempX < 0f && !leftTurn)
         {
             transform.Rotate(Vector3.up * 180f);
@@ -61,6 +86,7 @@ public class PlayerMovement : MonoBehaviour
                 firstRightWalk = true;
             }
         }
+        // References
         shadow.localScale = Vector3.one * 0.5f;
         boxCol.enabled = false;
         capsCol.enabled = true;
@@ -71,29 +97,71 @@ public class PlayerMovement : MonoBehaviour
             shadow.localScale = new Vector3(1f, 0.5f, 1f);
             capsCol.enabled = false;
             boxCol.enabled = true;
-            isCrouch = true;
+            if (!isCrouch)
+            {
+                crouchSound.Play();
+                isCrouch = true;
+            }
+            runSound.Stop();
         }
         // Run
         else if (Input.GetKey(KeyCode.LeftShift))
+        {
             rigidBody.velocity = new Vector3(x * walkSpeed * 2f, 0f, z * walkSpeed * 2f);
+            if (!isRun && (x != 0f || z != 0f))
+            {
+                runSound.Play();
+                isRun = true;
+            }
+            walkSound.Stop();
+        }
         // Normal
         else
         {
             rigidBody.velocity = new Vector3(x * walkSpeed, 0f, z * walkSpeed);
-            isCrouch = false;
+            if (isRun)
+            {
+                runSound.Stop();
+                walkSound.Play();
+                isRun = false;
+            }
+            if (isCrouch)
+            {
+                crouchSound.Play();
+                isCrouch = false;
+            }
         }
+        // Turn Left or Right
         tempX = x;
+        tempZ = z;
+        // Set Animator
         playerControlAnim.SetFloat("WalkSpeed", Mathf.Abs(x * walkSpeed));
         playerControlAnim.SetFloat("ZWalkSpeed", Mathf.Abs(z * walkSpeed));
         playerControlAnim.SetBool("IsCrouching", isCrouch);
         // Jump
-        if (isGround && Input.GetKey(KeyCode.Space))
+        if (isGround)
         {
-            rigidBody.velocity += new Vector3(x * walkSpeed, 10f * jumpForce, 0f);
-            playerControlAnim.SetBool("IsJumping", true);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rigidBody.velocity += new Vector3(x * walkSpeed, 10f * jumpForce, 0f);
+                playerControlAnim.SetBool("IsJumping", true);
+                startJump = true;
+            }
+            else if (startLand) OnLand.Invoke();
+            if (startJump)
+            {
+                jumpSound.Play();
+                startJump = false;
+                startLand = true;
+            }
         }
-        else if (!isGround) rigidBody.velocity += new Vector3(x * walkSpeed, -jumpForce, 0f);
-        else if (isGround) OnLand.Invoke();
+        else
+        {
+            walkSound.Stop();
+            runSound.Stop();
+            crouchSound.Stop();
+            rigidBody.velocity += new Vector3(x * walkSpeed, -jumpForce, 0f);
+        }
     }
     void FixedUpdate()
     {
